c72303bf558e57cc9bdd080e18ee7dd40de3b5bc
Yann Burrett <yann.let@gmail.com>
2022-03-12 13:15:27 +0000

    4 Setting Up The Controls

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,19 @@ func _ready():
 	randomize()
 
 
+func _unhandled_input(event):
+	if event.is_action_pressed("Fire"):
+		print("Fire")
+	if event.is_action_pressed("Left"):
+		print("Left")
+	if event.is_action_pressed("Right"):
+		print("Right")
+	if event.is_action_pressed("Nudge_Left"):
+		print("Nudge_Left")
+	if event.is_action_pressed("Nudge_Right"):
+		print("Nudge_Right")
+
+
 func _on_Timer_timeout():
 	$AnimatedSprite.frame = 0
 	var animations = ["blink", "flap", "look"]
