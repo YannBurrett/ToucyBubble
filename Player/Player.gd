0ba47bac939a144731ba2df6c53907928c1e6034
Yann Burrett <yann.let@gmail.com>
2022-03-12 13:27:47 +0000

    5 Animating The Cannon

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,21 +1,27 @@
 extends Node2D
 
+const MAX_ANGLE = 70
+const ROTATION_SPEED = 1
 
 func _ready():
 	randomize()
 
 
+func _process(_delta):
+	var rot = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	if abs($Aimer.rotation_degrees + rot * ROTATION_SPEED) <= MAX_ANGLE:
+		$Aimer.rotation_degrees += rot * ROTATION_SPEED
+
+
 func _unhandled_input(event):
 	if event.is_action_pressed("Fire"):
 		print("Fire")
-	if event.is_action_pressed("Left"):
-		print("Left")
-	if event.is_action_pressed("Right"):
-		print("Right")
 	if event.is_action_pressed("Nudge_Left"):
-		print("Nudge_Left")
+		if abs($Aimer.rotation_degrees -1) <= MAX_ANGLE:
+			$Aimer.rotation_degrees -= 1
 	if event.is_action_pressed("Nudge_Right"):
-		print("Nudge_Right")
+		if abs($Aimer.rotation_degrees +1) <= MAX_ANGLE:
+			$Aimer.rotation_degrees += 1
 
 
 func _on_Timer_timeout():
