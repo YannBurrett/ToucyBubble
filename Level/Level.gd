3477dcf0d20c5da4ebec3651cd9d9d9ffff43898
Yann Burrett <yann.let@gmail.com>
2022-03-30 11:32:56 +0100

    13 Matching Bubbles

--- Level/Level.gd
+++ Level/Level.gd
@@ -5,6 +5,8 @@ signal spawn_bubble
 signal prepare_bubble
 
 var PlayerLoc: Vector2
+var matching_bubbles = []
+var bubbles_checked = []
 
 const MAP_X = 19
 const MAP_Y = 13
@@ -41,7 +43,51 @@ func bubble_stopped(pos, bubble, type):
 	pos = $TileMap.world_to_map(pos)
 	$TileMap.set_cell(pos.x, pos.y, type)
 	bubble.queue_free()
+	
+	###check for matches
+	matching_bubbles = [pos]
+	bubbles_checked = []
+	$Timer.start()
+	check_neighbours(pos)
 
+
+func check_neighbours(cell):
+	bubbles_checked.append(cell)
+	var neighbours = get_my_neighbours(cell)
+	var used_cells = $TileMap.get_used_cells()
+	for n in neighbours:
+		if not bubbles_checked.has(cell+n) and used_cells.has(cell+n):
+			if $TileMap.get_cellv(cell+n) == $TileMap.get_cellv(cell):
+				matching_bubbles.append(cell+n)
+				check_neighbours(cell+n)
+
+
+func get_my_neighbours(cell):
+	var neighbours = [
+			Vector2(-1,-1), Vector2(0,-1),
+			Vector2(-1,0), Vector2(1,0),
+			Vector2(-1,1), Vector2(0,1)
+			]
+	var offset_neighbours = [
+			Vector2(0,-1), Vector2(1,-1),
+			Vector2(-1,0), Vector2(1,0),
+			Vector2(0,1), Vector2(1,1)
+		]
+	if int(cell.y) %2:
+		return offset_neighbours
+	else:
+		return neighbours
+
+
+func _on_Timer_timeout():
+	check_bubble_counter()
+
+
+func check_bubble_counter():
+	if matching_bubbles.size() > 2:
+		# increment score
+		for bubble in matching_bubbles:
+			$TileMap.set_cellv(bubble, -1)
 
 
 
